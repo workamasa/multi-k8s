5aaf209ac608f6a4cd5a43e8233b9682c10d693a
Abubakar Amasa <abubakaramasa@Joshuas-MacBook-Pro.local>
2022-10-01 23:54:24 +0100

    feat

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            FIB CALCULATOR2
+            FIB CALCULATOR Kubernetees
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
